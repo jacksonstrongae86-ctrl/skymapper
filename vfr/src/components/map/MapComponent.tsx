fe13032639644c829c9c42a9a602bc41cee92ff3
MatiasJF <matias.jackson.f@gmail.com>
2025-03-29 16:42:39 +0100

    fix map compoent

--- vfr/src/components/map/MapComponent.tsx
+++ vfr/src/components/map/MapComponent.tsx
@@ -6,9 +6,9 @@ import {
   Polyline,
   useMapEvents,
 } from "react-leaflet";
-import { Waypoint } from "../../utils/types"; // Ajusta la ruta si es necesario
-import L, { LeafletMouseEvent } from "leaflet";
-import WaypointInput from "../waypoints/WaypointInput";
+import { Waypoint } from "../../utils/types";
+import   { LeafletMouseEvent } from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 type MapComponentProps = {
   onMapClick: (e: LeafletMouseEvent) => void;
@@ -23,140 +23,95 @@ const MapComponent: React.FC<MapComponentProps> = ({
   setWaypoints,
   mapType,
 }) => {
-  const defaultTAS = 100;
+  // const mapRef = useRef<L.Map | null>(null);
+
   const validMapTypes = ["street", "sat", "hybrid", "terrain"];
-  const mapTypeUrl = validMapTypes.includes(mapType) ? mapType : "sat"; // Establecer un valor por defecto si no es válido
-  console.log("type", mapTypeUrl);
+  const mapTypeUrl = validMapTypes.includes(mapType) ? mapType : "sat";
+
+  // Force Leaflet to recalculate the map layout
+  // useEffect(() => {
+  //   if (mapRef.current) {
+  //     mapRef.current.invalidateSize();
+  //   }
+  // }, [mapRef]);
+
   return (
-    <div className="">
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      style={{ height: "100vh", width: "100%" }}
-      crs={L.CRS.EPSG3857}
-    >
-      {/* Aquí se maneja el clic en el mapa para agregar waypoints */}
-      <MapEvents
-        onMapClick={onMapClick}
-        waypoints={waypoints}
-        setWaypoints={setWaypoints}
-      />
-
-      <TileLayer
-        url={
-          mapTypeUrl === "street"
-            ? "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            : mapTypeUrl === "sat"
-            ? "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-            : mapTypeUrl === "hybrid"
-            ? "https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
-            : mapTypeUrl === "terrain"
-            ? "https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}"
-            : "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        }
-        attribution="&copy; OpenStreetMap contributors"
-        subdomains={
-          mapTypeUrl === "hybrid" || mapTypeUrl === "terrain"
-            ? ["mt0", "mt1", "mt2", "mt3"]
-            : ["a", "b", "c"]
-        }
-
-      />
-
-      {waypoints.map((waypoint, index) => (
-        <Marker
-          key={index}
-          position={waypoint.position}
-          draggable={true}
-          eventHandlers={{
-            dragend: (e) => {
-              const updatedWaypoints = [...waypoints];
-              updatedWaypoints[index] = {
-                ...waypoint,
-                position: [e.target._latlng.lat, e.target._latlng.lng],
-              };
-              setWaypoints(updatedWaypoints);
-            },
-          }}
+    <div className="relative w-full h-full">
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{ height: "100%", width: "100%" }}
+        // whenReady={(mapInstance) => {
+        //   mapRef.current = mapInstance; // Directly assign the map instance
+        // }}
+      >
+        {/* Handle map click events */}
+        <MapEvents
+          onMapClick={onMapClick}
+          waypoints={waypoints}
+          setWaypoints={setWaypoints}
         />
-      ))}
 
-      {waypoints.length > 1 && (
-        <Polyline positions={waypoints.map((wp) => wp.position)} color="blue" />
-      )}
-      {waypoints.map((waypoint, index) => (
-        <WaypointInput
-          key={index}
-          index={index + 1}
-          type={waypoint.type}
-          altitude={waypoint.altitude}
-          ias={waypoint.ias}
-          altitudeChange={waypoint.altitudeChange}
-          rocRod={waypoint.rocRod}
-          iasClimbDescent={waypoint.iasClimbDescent}
-          onTypeChange={(value) => {
-            const updatedWaypoints = [...waypoints];
-            updatedWaypoints[index] = { ...waypoint, type: value };
-            setWaypoints(updatedWaypoints);
-          }}
-          onAltitudeChange={(value) => {
-            const updatedWaypoints = [...waypoints];
-            updatedWaypoints[index] = {
-              ...waypoint,
-              altitude: parseFloat(value) || 0,
-            };
-            setWaypoints(updatedWaypoints);
-          }}
-          onIasChange={(value) => {
-            const updatedWaypoints = [...waypoints];
-            updatedWaypoints[index] = {
-              ...waypoint,
-              ias: parseFloat(value) || defaultTAS,
-            };
-            setWaypoints(updatedWaypoints);
-          }}
-          onAltitudeChangeChange={(value) => {
-            const updatedWaypoints = [...waypoints];
-            updatedWaypoints[index] = {
-              ...waypoint,
-              altitudeChange: parseFloat(value) || 0,
-            };
-            setWaypoints(updatedWaypoints);
-          }}
-          onRocRodChange={(value) => {
-            const updatedWaypoints = [...waypoints];
-            updatedWaypoints[index] = {
-              ...waypoint,
-              rocRod: parseFloat(value) || 500,
-            };
-            setWaypoints(updatedWaypoints);
-          }}
-          onIasClimbDescentChange={(value) => {
-            const updatedWaypoints = [...waypoints];
-            updatedWaypoints[index] = {
-              ...waypoint,
-              iasClimbDescent: parseFloat(value) || defaultTAS,
-            };
-            setWaypoints(updatedWaypoints);
-          }}
+        <TileLayer
+          url={
+            mapTypeUrl === "street"
+              ? "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              : mapTypeUrl === "sat"
+              ? "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              : mapTypeUrl === "hybrid"
+              ? "https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
+              : mapTypeUrl === "terrain"
+              ? "https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}"
+              : "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          }
+          attribution="&copy; OpenStreetMap contributors"
+          subdomains={
+            mapTypeUrl === "hybrid" || mapTypeUrl === "terrain"
+              ? ["mt0", "mt1", "mt2", "mt3"]
+              : ["a", "b", "c"]
+          }
         />
-      ))}
-    </MapContainer>
+
+        {waypoints.map((waypoint, index) => (
+          <Marker
+            key={index}
+            position={waypoint.position}
+            draggable={true}
+            eventHandlers={{
+              dragend: (e) => {
+                const updatedWaypoints = [...waypoints];
+                updatedWaypoints[index] = {
+                  ...waypoint,
+                  position: [e.target.getLatLng().lat, e.target.getLatLng().lng],
+                };
+                setWaypoints(updatedWaypoints);
+              },
+            }}
+          />
+        ))}
+
+        {waypoints.length > 1 && (
+          <Polyline
+            positions={waypoints.map((wp) => wp.position)}
+            color="blue"
+          />
+        )}
+      </MapContainer>
     </div>
   );
 };
 
-// Componente para manejar los eventos de clic
+// Component to handle map click events
 const MapEvents: React.FC<{
   onMapClick: (e: LeafletMouseEvent) => void;
   waypoints: Waypoint[];
   setWaypoints: React.Dispatch<React.SetStateAction<Waypoint[]>>;
 }> = ({ onMapClick }) => {
   useMapEvents({
-    click: onMapClick, // Usamos el evento de clic para añadir waypoints
+    click: onMapClick,
   });
 
-  return null; // Este componente no renderiza nada, solo maneja eventos
+  return null;
 };
 
 export default MapComponent;
