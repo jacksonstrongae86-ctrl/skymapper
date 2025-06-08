f82074f5191dd21486b2eea0b8edac70732b1e19
MatiasJF <matias.jackson.f@gmail.com>
2025-06-08 22:52:30 +0200

    icon

--- vfr/src/pages/_document.tsx
+++ vfr/src/pages/_document.tsx
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon2.ico" />
-        <link rel="shortcut icon" href="/favicon2.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body className="antialiased">
         <Main />
