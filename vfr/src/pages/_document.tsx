f54c174eb8286802141659b4876e8475a4e9ff74
MatiasJF <matias.jackson.f@gmail.com>
2025-06-09 01:10:32 +0200

    google analytics

--- vfr/src/pages/_document.tsx
+++ vfr/src/pages/_document.tsx
@@ -6,6 +6,21 @@ export default function Document() {
       <Head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-J22C85TW7T`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2GBJG8M351');
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
         <Main />
