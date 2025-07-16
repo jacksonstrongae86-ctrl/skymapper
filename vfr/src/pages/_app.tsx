8b888509b9d69afc1782c163968e818ba4551c40
MatiasJF <matias.jackson.f@gmail.com>
2025-07-16 16:19:53 +0200

    daily sync

--- vfr/src/pages/_app.tsx
+++ vfr/src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "../utils/ThemeContext";
+import { useEffect } from "react";
+import { startDailySync } from "../services/dailySync";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      startDailySync();
+    }
+  }, []);
   return (
     <ThemeProvider>
       <Component {...pageProps} />
