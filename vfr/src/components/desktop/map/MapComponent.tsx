55cc242896f6122d952aeb888d5b1f24fbd6d382
MatiasJF <matias.jackson.f@gmail.com>
2025-07-25 19:16:49 +0200

    https in map components

--- vfr/src/components/desktop/map/MapComponent.tsx
+++ vfr/src/components/desktop/map/MapComponent.tsx
@@ -215,14 +215,14 @@ const MapComponent: React.FC<MapComponentProps> = ({
       <TileLayer
         url={
           mapTypeUrl === "street"
-            ? "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+            ? "https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
             : mapTypeUrl === "sat"
             ? "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
             : mapTypeUrl === "hybrid"
             ? "https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
             : mapTypeUrl === "terrain"
             ? "https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}"
-            : "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+            : "https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
         }
         attribution="&copy; OpenStreetMap contributors"
         subdomains={["mt0", "mt1", "mt2", "mt3"]}
