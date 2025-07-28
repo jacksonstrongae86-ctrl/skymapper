8f6ccdf52a95f8e738338792c741c16cf1dd60aa
MatiasJF <matias.jackson.f@gmail.com>
2025-07-28 15:46:58 +0200

    .i.

--- vfr/src/pages/_app.tsx
+++ vfr/src/pages/_app.tsx
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
         // api_host: "/ingest",
         // ui_host: "/ingest",
         api_host: "https://eu.i.posthog.com", // Endpoints oficiales
-        ui_host: "https://eu.posthog.com",
+        ui_host: "https://eu.i.posthog.com",
 
         opt_out_capturing_by_default: !hasAnalyticsConsent,
         persistence: hasAnalyticsConsent ? "localStorage+cookie" : "memory",
