1684fa722d3ef44a8c589794a297c489606c72be
MatiasJF <matias.jackson.f@gmail.com>
2025-12-23 22:49:28 +0100

    return to original structure but fix on versions

---
+++ vfr/next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+
+  // Proxy reverso para PostHog
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/ingest/static/:path*",
+  //       destination: "https://eu-assets.i.posthog.com/static/:path*",
+  //     },
+  //     {
+  //       source: "/ingest/api/:path*",
+  //       destination: "https://eu.i.posthog.com/api/:path*",
+  //     },
+  //     {
+  //       source: "/ingest/:path*",
+  //       destination: "https://eu.i.posthog.com/:path*",
+  //     },
+  //     {
+  //       source: "/ingest",
+  //       destination: "https://eu.i.posthog.com",
+  //     },
+  //   ];
+  // },
+};
+
+export default nextConfig;
